f51381ce5c5074f6a0390660aca137d313d0f497
AnnaShell <anna.tityushina@yandex.ru>
2025-11-19 13:53:17 +0300

    dsp 7

--- src/api/courses.js
+++ src/api/courses.js
@@ -49,6 +49,7 @@ const coursesData = [
             { id: 4, title: "Мел-кепстральные коэффициенты (MFCC)", filename: "dsp_4.html" },
             { id: 5, title: "Коэффициенты линейного предсказания (LPC)", filename: "dsp_5.html" },
             { id: 6, title: "Введение в машинное обучение: линейная регрессия", filename: "dsp_6.html" },
+            { id: 7, title: "Введение в машинное обучение: задача классификации", filename: "dsp_7.html" },
         ]
     },
     {
